5eca07433b9e168852e60c2be79259fdcd1941df
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-02 01:46:36 +0600

    banner

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-red-200 sm:mt-5 md:mt-20 md:flex justify-around'>
-           <div className='text-center bg-slate-400 w-full'>
-            text
-           </div>
+        <div className='sm:mt-5 md:mt-4 md:flex justify-around'>
+            <div className='text-left  w-full flex flex-col justify-center '>
+                <h1 className='text-3xl font-bold text-red-500 mb-3'>
+                    Lorem ipsum dolor sit amet.
+                </h1>
+                <p className='text-left'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quam fuga minima ab labore dolorum vero necessitatibus quis sunt omnis.
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quam fuga minima ab labore dolorum vero necessitatibus quis sunt omnis.
+                </p>
+                <button className='btn-error text-white mt-5 w-[25%]'>btn</button>
+            </div>
 
-           <div className='w-full text-center'>
-            image
-           </div>
+            <div className='w-full '>
+                <img src="https://i.ibb.co/wL1qrCx/Untitled-design-5.png" alt="" />
+            </div>
         </div>
     );
 };
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-blue-100
+            <div className="navbar
             flex justify-between mb-5">
                 <div className="">
                     <img className='h-10' src="https://i.ibb.co/9ssRxKg/Untitled-design-2-prev-ui.png" alt="" />
