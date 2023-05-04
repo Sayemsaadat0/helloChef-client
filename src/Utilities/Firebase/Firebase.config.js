8720e389e5bf956ca72dda70643048b5f109d011
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-04 16:39:19 +0600

    environment variable

--- src/Utilities/Firebase/Firebase.config.js
+++ src/Utilities/Firebase/Firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyCSrp2a6FBGUtfIL103S46GMKCwZU3tnHk",
-  authDomain: "hellochef-e2dda.firebaseapp.com",
-  projectId: "hellochef-e2dda",
-  storageBucket: "hellochef-e2dda.appspot.com",
-  messagingSenderId: "1051566971372",
-  appId: "1:1051566971372:web:dafdb62b464be5b44e88c9"
+  apiKey: import.meta.env.VITE_apiKey ,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
