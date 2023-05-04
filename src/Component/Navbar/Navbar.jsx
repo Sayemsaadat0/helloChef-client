53ed75dad7a81a99dddb7565067ee3cc84a0d775
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-04 11:42:04 +0600

    active routes

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import './Navbar.css'
+import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
@@ -20,19 +21,21 @@ const Navbar = () => {
                     <img className='h-10' src="https://i.ibb.co/9ssRxKg/Untitled-design-2-prev-ui.png" alt="" />
                 </div>
                 <div className=' gap-4'>
-                        <Link to='/'>Home</Link>
-                        <Link to='/blogs'>Blogs</Link>
+                        <NavLink to='/'>Home</NavLink>
+                        <NavLink to='/blogs'>Blogs</NavLink>
                 </div>
                 <div>
                    {
                     user ?  
                    <>
-                    <img className='mr-5 h-10 rounded-full' src={user.photoURL} alt="profile" />
-                    <button onClick={handlelogout} className='bg-red-500 text-white w-20 text-center'>
+                    <div className='tooltip tooltip-error' data-tip={user.displayName}> 
+                    <img className='mr-5 h-10 rounded-full'  src={user.photoURL} alt=""/>
+                    </div>
+                    <button onClick={handlelogout} className=' text-red-500 hover:shadow-lg w-20 text-center'>
                         log out
                     </button>
                    </> : 
-                    <Link className='bg-red-500 text-white w-20 text-center' to='/login'>Login</Link>
+                    <NavLink className='bg-red-500 text-white w-20 text-center' to='/login'>Login</NavLink>
                    }
                 </div>
             </div>
